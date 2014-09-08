3da349343e0070d9b8fad3b40bbcf7348da2b70f
Katsuya Kuwabara <kkuwabara@mediamath.com>
2014-09-08 10:49:34 +0900

    added sample code

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
-class counter(object):
-    def __init__(self):
-        self.num = 3
-    def countup(self):
-        self.num += 1
-        return self.num
-    def countdown(self):
-        self.num -= 1
-        if self.num == 0:
-            return "fire!"
-        return self.num
+class Island:
+    def __init__(self, name, opens, closes, next=None):
+        self.name = name
+        self.opens = opens
+        self.closes = closes
+        self.next = next
+    
+    def display(self):
+        print('name:{0} working time:{1}-{2}'.format(self.name, self.opens, self.closes))
+        if self.next:
+            self.next.display()
 
-rocket = counter()
-print rocket.countdown()
-print rocket.countdown()
-print rocket.countdown()
+amity = Island('amity', '9:00', '17:00')
+craggy = Island('craggy', '9:00', '17:00')
+isla_nublar = Island('isla_nublar', '9:00', '17:00')
+shutter = Island('shutter', '9:00', '17:00')
+skull = Island('skull', '9:00', '17:00')
+
+amity.next = craggy
+craggy.next = isla_nublar
+isla_nublar.next = shutter
+shutter.next = skull
+
+amity.display()
